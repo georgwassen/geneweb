78578994d17201ce8f3b0d8d032eecf47596328a
(no author) <(no author)@56c106c9-b585-4593-932e-8dfca04823a8>
1999-03-08 11:19:48 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-2.1.4'.

--- src/family.mli
+++
@@ -1,4 +0,0 @@
-(* $Id: family.mli,v 2.1 1999-03-08 11:18:37 ddr Exp $ *)
-(* Copyright (c) 1999 INRIA *)
-
-value family : Config.config -> Def.base -> unit;
--- src/merge.mli
+++
@@ -1,9 +0,0 @@
-(* $Id: merge.mli,v 2.1 1999-03-08 11:18:51 ddr Exp $ *)
-(* Copyright (c) 1999 INRIA *)
-
-open Def;
-open Config;
-
-value print_someone : config -> base -> base_person -> unit;
-value print : config -> base -> base_person -> unit;
-
